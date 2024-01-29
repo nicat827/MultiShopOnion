a4d5f8072e6e65437798adad3a5c742ae69db08a
nicat827 <nici.mecidov@list.ru>
2024-01-29 19:27:39 +0400

    Color and Size entities created

---
+++ Core/MultishopOnion.Application/Abstractions/Repositories/IColorRepository.cs
@@ -0,0 +1,13 @@
+﻿using MultishopOnion.Application.Abstractions.Repositories.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultishopOnion.Application.Abstractions.Repositories
+{
+    public interface IColorRepository:IGenericRepository<Color>
+    {
+    }
+}
---
+++ Core/MultishopOnion.Domain/Entities/Color.cs
@@ -0,0 +1,13 @@
+﻿using MultishopOnion.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultishopOnion.Domain.Entities
+{
+    public class Color: BaseNameableEntity
+    {
+    }
+}
